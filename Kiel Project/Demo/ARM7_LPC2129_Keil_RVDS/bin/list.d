ad7d34ee834c6d6e505628e490ad80f895017bc0
unknown <basma.ashraf.muhamad@gmail.com>
2023-02-12 23:27:18 +0200

    intial commit

---
+++ Kiel Project/Demo/ARM7_LPC2129_Keil_RVDS/bin/list.d
@@ -0,0 +1,17 @@
+.\bin\list.o: ..\..\Source\list.c
+.\bin\list.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\bin\list.o: ..\..\Source\include\FreeRTOS.h
+.\bin\list.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\bin\list.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\bin\list.o: .\FreeRTOSConfig.h
+.\bin\list.o: C:\Keil_v5\ARM\INC\Philips\lpc21xx.h
+<<<<<<< HEAD
+.\bin\list.o: ..\..\Starter_Files_V1\Starter_Files_V1\header\GPIO.h
+=======
+>>>>>>> e62a9df9d2219aaa77dcf4e90bcc898a55488de3
+.\bin\list.o: ..\..\Source\include\projdefs.h
+.\bin\list.o: ..\..\Source\include\portable.h
+.\bin\list.o: ..\..\Source\include\deprecated_definitions.h
+.\bin\list.o: ..\..\Source\include\..\..\Source\portable\RVDS\ARM7_LPC21xx\portmacro.h
+.\bin\list.o: ..\..\Source\include\mpu_wrappers.h
+.\bin\list.o: ..\..\Source\include\list.h
